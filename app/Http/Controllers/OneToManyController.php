923066f4580f94e783bc06c9ca03663994d7da02
diegocar448 <diegocar448@hotmail.com>
2018-09-18 09:29:13 -0300

    OneToMany/Controller

--- app/Http/Controllers/OneToManyController.php
+++ app/Http/Controllers/OneToManyController.php
@@ -3,11 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Country;
+use App\Models\State;
 
 class OneToManyController extends Controller
 {
     public function oneToMany()
     {
-        
+        $keySearch = 'a';
+
+        //$country = Country::where('name', 'Brasil')->get()->first();
+        $countries = Country::where('name', 'LIKE', "%{$keySearch}%")->get();
+        foreach($countries as $country)
+        {        
+            echo "<b>{$country->name}</b>"; 
+
+            $states = $country->states()->get();
+
+            foreach($states as $state)
+            {
+                echo "<br> {$state->initials} - {$state->name}";
+            }
+            echo '<hr>';
+
+        }
     }
 }
--- app/Models/Country.php
+++ app/Models/Country.php
@@ -4,15 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Location;
+use App\Models\State;
 
 class Country extends Model
 {
-    protected $fillable = [
-        'name'
-    ];
+    protected $fillable = ['name'];
 
     public function location()
     {
         return $this->hasOne(Location::class);        
+    }
+
+    public function states()
+    {
+        return $this->hasMany(State::class);
     }
 }
